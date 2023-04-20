c21afdbf746b483345d2b3fbf80a1105854bddf0
nandadev <nandadev.rajeevmenon@student.griffith.ie>
2023-04-21 00:04:46 +0100

    Make header n footer into components

---
+++ components/footer.php
@@ -0,0 +1,81 @@
+</div>
+
+<footer class="site-footer">
+  <div class="blue-bottom-border w-100">
+    <div class="d-flex px-5 py-4 w-75 mx-auto">
+      <div class="ms-5 me-auto d-none d-lg-block">
+        <span>Connect with us on these social networks:</span>
+      </div>
+      <div class="me-5 d-flex w-50 align-items-center justify-content-end">
+        <a><img src="images/icons/instagram.svg" alt="Instagram Logo" class="me-4"></a>
+        <a><img src="images/icons/facebook.svg" alt="Facebook Logo"></a>
+        <a><img src="images/icons/github.svg" alt="Github Logo" class="ms-4"></a>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div class="container text-center text-md-start mt-5">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4 footer-brand">
+            Tiny Treasures
+          </h6>
+          <p>
+            At Tiny Treasures, we're committed to providing a nurturing and fun environment for your child to grow, learn, and explore the world around them. Contact us today to learn more.
+          </p>
+        </div>
+      
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Angular</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">React</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vue</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Laravel</a>
+          </p>
+        </div>
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p>
+            <img class="me-3" src="images/icons/house-fill.svg" alt="Address icon">
+            Belgard, Dublin, D24XC2G
+          </p>
+          <p>
+            <img class="me-3" src="images/icons/envelope-fill.svg" alt="email icon">
+            info@example.com
+          </p>
+          <p> <img class="me-3" src="images/icons/telephone-fill.svg" alt="phone icon"> + 01 234 567 88</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<script src="index.js"></script>
+</body>
+</html>
